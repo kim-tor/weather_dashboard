fb250d39ae213969311788bb56dfdccc435eb43b
Kim Torres <kimberley.5118@gmail.com>
2020-10-07 19:48:18 -0400

    minor formatting edits

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function pageload() {
     var psearch = $("<div>");
     psearch.append(searchDiv);
     // console.log(psearch);
-   $("#searchHistory").prepend(psearch);
+    $("#searchHistory").prepend(psearch);
 }
 
 function citySearch(city) {
@@ -36,86 +36,86 @@ function citySearch(city) {
         url: currentQueryURL,
         method: "GET"
     }).then(function (response) {
-            // console.log(currentQueryURL);
+        // console.log(currentQueryURL);
+        // console.log(response);
+        $("#current").empty();
+        var date = moment().format("L");
+
+        //create elements to hold data we want to retrieve
+        var cityEl = $("<h2>").text(response.name);
+        var displayDate = cityEl.append(" " + date);
+
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var tempEl = $("<p>").text("Temperature: " + tempF.toFixed(2));
+        var humidityEl = $("<p>").text("Humidity: " + response.main.humidity);
+        var windEl = $("<p>").text("Wind Speed: " + response.wind.speed);
+
+
+
+        var newDiv = $("<div>");
+        newDiv.append(displayDate, tempEl, humidityEl, windEl)
+        $("#current").html(newDiv);
+
+
+        //api call just to get uv index
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        var uvURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=499cde1e1f8370961787055f87ad783b";
+
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function (response) {
             // console.log(response);
-            $("#current").empty();
-            var date = moment().format("L");
+            $("#uv").empty();
+            var uvResult = response.value;
+            var uvEl = $("<p>").text("UV Index: " + uvResult);
 
-            //create elements to hold data we want to retrieve
-            var cityEl = $("<h2>").text(response.name);
-            var displayDate = cityEl.append(" " + date);
+            $("#uv").html(uvEl);
 
-            var tempF =(response.main.temp - 273.15) * 1.80 + 32;
-            var tempEl = $("<p>").text("Temperature: " + tempF.toFixed(2));
-            var humidityEl = $("<p>").text("Humidity: " + response.main.humidity);
-            var windEl = $("<p>").text("Wind Speed: " + response.wind.speed);
-
-          
-
-            var newDiv = $("<div>");
-            newDiv.append(displayDate, tempEl, humidityEl, windEl)
-            $("#current").html(newDiv);
-
-
-            //api call just to get uv index
-            var lat = response.coord.lat;
-            var lon = response.coord.lon;
-            var uvURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=499cde1e1f8370961787055f87ad783b";
-
-            $.ajax({
-                url: uvURL,
-                method: "GET"
-            }).then(function (response) {
-                // console.log(response);
-                $("#uv").empty();
-                var uvResult = response.value;
-                var uvEl = $("<p>").text("UV Index: " + uvResult);
-
-                $("#uv").html(uvEl);
-
-            });
         });
+    });
 
     //api call for 5 day forecase
     $.ajax({
         url: fiveDayQueryURL,
         method: "GET"
     }).then(function (response) {
-            // console.log(fiveDayQueryURL);
-            // console.log(response);
+        // console.log(fiveDayQueryURL);
+        // console.log(response);
 
-            //variable to store an array of the results
-            var results = response.list;
+        //variable to store an array of the results
+        var results = response.list;
 
-            $("#five-day").empty();
+        $("#five-day").empty();
 
-            // create a for loop to iterate through the 5 day responses, creating a div and individual variables for each element
-            for (var i = 0; i < results.length; i+=8) {
-                var fiveDiv = $("<div>");
+        // create a for loop to iterate through the 5 day responses, creating a div and individual variables for each element
+        for (var i = 0; i < results.length; i += 8) {
+            var fiveDiv = $("<div>");
 
-                // variables to hold data
-                var date = results[i].dt_txt;
-                var shortDate = date.substr(0,10);
-                var temp = results[i].main.temp;
-                var humidity = results[i].main.humidity;
+            // variables to hold data
+            var date = results[i].dt_txt;
+            var shortDate = date.substr(0, 10);
+            var temp = results[i].main.temp;
+            var humidity = results[i].main.humidity;
 
-                // console.log(shortDate);
-                // console.log(temp);
-                // console.log(humidity);
+            // console.log(shortDate);
+            // console.log(temp);
+            // console.log(humidity);
 
-                // create elements for html
-                var dateLabel = $("<h5>").text(shortDate);
+            // create elements for html
+            var dateLabel = $("<h5>").text(shortDate);
 
-                var tempLabel = $("<p>").text("Temperature: " + temp);
-                var humidtyLabel = $("<p>").text("Humidity: " + humidity);
+            var tempLabel = $("<p>").text("Temperature: " + temp);
+            var humidtyLabel = $("<p>").text("Humidity: " + humidity);
 
 
-                fiveDiv.append(dateLabel);
-                fiveDiv.append(tempLabel);
-                fiveDiv.append(humidtyLabel);
-                $("#five-day").append(fiveDiv);
+            fiveDiv.append(dateLabel);
+            fiveDiv.append(tempLabel);
+            fiveDiv.append(humidtyLabel);
+            $("#five-day").append(fiveDiv);
 
-            }
-        });
-    }
-    pageload();
+        }
+    });
+}
+pageload();
