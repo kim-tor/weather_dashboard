dc1ebff842eb8e33e3bb1a5a96268c004dc9a93e
Kim Torres <kimberley.5118@gmail.com>
2020-10-07 19:42:18 -0400

    more edits to the js and the html, updated the read me

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # weather_dashboard
+
+# Description
+
+For this assignment the goal was to create a weather dashboard that allowed you as the traveler to input any city
+and it would retrieve the data on the current weather as well as the 5 day forecast. The website should also save
+your searches on the side and allow them to be clickable if you were to want to check an area that you had recently
+looked at. 
+
+# Installation
+To begin I created the basic html that was going to be needed to hold all of the data that was going to be retrieved. 
+Using Jquery I created the elements and then pushed them into the html. Using ajax calls I was able to gather the data 
+from the open weather map api. I also used moment.js again to obtain the date information. 
+
+# Problems
+I was unsure of how to display the weather icons. I saw that they were part of the data that came from the api response.
+I also had an issue with generating my list of searches. Upon reloading the page my last search was the only one that came up. 
+I tried creating an array to hold all the searches and then thought that they would all load however that was not the case. 
+I also couldn't get my icon to work for the search button. I feel like I had a lot of little nuances that I struggled to figure out.
+
+# Credits
+One of the memebers for my group project (Anna C) helped us (our group) with how to get started with grabbing the UV index. 
+I also used the cors link that was provided by one of our TA's. When I first started the assignment I was able to do api calls
+but towards the end of completing it it started giving me that error. By inputting that beginning to my URL I was able to access
+my api call. 
+
+https://help.highbond.com/helpdocs/analytics/142/scripting-guide/en-us/Content/lang_ref/functions/r_substr.htm
+
+\https://fontawesome.com/icons/search-location?style=solid
+
+https://momentjs.com/
--- script.js
+++ script.js
@@ -11,22 +11,21 @@ $("#select-city").on("click", function (event) {
     //save the response
     var textContent = $(this).siblings("input").val();
     var storearr = [];
+    storearr.push(textContent);
     localStorage.setItem("city", JSON.stringify(storearr));
     citySearch(cityName);
-    // pageLoad();
-
 });
 
 // function to retrieve data on page load
-
+// i feel like this should be saving my searches but it is not working 
 function pageload() {
     var lastSearch = JSON.parse(localStorage.getItem("city"));
     var searchDiv = $("<button>").text(lastSearch);
     var psearch = $("<div>");
     psearch.append(searchDiv);
-    $("searchHistory").prepend(psearch);
+    // console.log(psearch);
+   $("#searchHistory").prepend(psearch);
 }
-
 
 function citySearch(city) {
     var currentQueryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=499cde1e1f8370961787055f87ad783b";
@@ -36,20 +35,22 @@ function citySearch(city) {
     $.ajax({
         url: currentQueryURL,
         method: "GET"
-    })
-        .then(function (response) {
-            console.log(currentQueryURL);
-            console.log(response);
+    }).then(function (response) {
+            // console.log(currentQueryURL);
+            // console.log(response);
             $("#current").empty();
             var date = moment().format("L");
 
             //create elements to hold data we want to retrieve
             var cityEl = $("<h2>").text(response.name);
             var displayDate = cityEl.append(" " + date);
-            var tempEl = $("<p>").text("Temperature: " + response.main.temp);
+
+            var tempF =(response.main.temp - 273.15) * 1.80 + 32;
+            var tempEl = $("<p>").text("Temperature: " + tempF.toFixed(2));
             var humidityEl = $("<p>").text("Humidity: " + response.main.humidity);
             var windEl = $("<p>").text("Wind Speed: " + response.wind.speed);
-            // var currentWeather = response.weather[0].main;
+
+          
 
             var newDiv = $("<div>");
             newDiv.append(displayDate, tempEl, humidityEl, windEl)
@@ -65,11 +66,12 @@ function citySearch(city) {
                 url: uvURL,
                 method: "GET"
             }).then(function (response) {
-                $(".uv").empty();
+                // console.log(response);
+                $("#uv").empty();
                 var uvResult = response.value;
-                var uvEl = $("<button>").text("UV Index: " + uvResult);
+                var uvEl = $("<p>").text("UV Index: " + uvResult);
 
-                $(".uv").html(uvEl);
+                $("#uv").html(uvEl);
 
             });
         });
@@ -78,10 +80,42 @@ function citySearch(city) {
     $.ajax({
         url: fiveDayQueryURL,
         method: "GET"
-    })
-        .then(function (response) {
-            console.log(fiveDayQueryURL);
-            console.log(response);
+    }).then(function (response) {
+            // console.log(fiveDayQueryURL);
+            // console.log(response);
 
-        })
-};
+            //variable to store an array of the results
+            var results = response.list;
+
+            $("#five-day").empty();
+
+            // create a for loop to iterate through the 5 day responses, creating a div and individual variables for each element
+            for (var i = 0; i < results.length; i+=8) {
+                var fiveDiv = $("<div>");
+
+                // variables to hold data
+                var date = results[i].dt_txt;
+                var shortDate = date.substr(0,10);
+                var temp = results[i].main.temp;
+                var humidity = results[i].main.humidity;
+
+                // console.log(shortDate);
+                // console.log(temp);
+                // console.log(humidity);
+
+                // create elements for html
+                var dateLabel = $("<h5>").text(shortDate);
+
+                var tempLabel = $("<p>").text("Temperature: " + temp);
+                var humidtyLabel = $("<p>").text("Humidity: " + humidity);
+
+
+                fiveDiv.append(dateLabel);
+                fiveDiv.append(tempLabel);
+                fiveDiv.append(humidtyLabel);
+                $("#five-day").append(fiveDiv);
+
+            }
+        });
+    }
+    pageload();
